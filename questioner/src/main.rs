2d655a0fd13ec661237604845297e4d86d04b219
Aoi-Takahashi <pythondevelop.forbusines@gmail.com>
2025-01-02 12:17:12 +0900

    feat: implement questioner

--- questioner/src/main.rs
+++ questioner/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::{Client, Error, RequestBuilder, Url};
+use std::io;
+use url::ParseError;
+
+fn create_url(path: String) -> Result<Url, ParseError> {
+    let base_url: String = "http://127.0.0.1:8080".to_string();
+    let url = base_url + &path;
+    Url::parse(&url)
+}
+
+fn read_buffer() -> String {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Failed to read line.");
+    buffer
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let client = Client::new();
+
+    println!("Welcome to Theme Reasoning!");
+
+    println!("Enter the question! (press Enter after typing)");
+    let question = read_buffer();
+    let post_theme_url = create_url("/questions".to_string()).unwrap();
+    let request_builder: RequestBuilder = client.post(post_theme_url).body(question);
+    let body = request_builder.send().await?.text().await?;
+    println!("{}", body); //TODO: Unnecessary code to be deleted later
+
+    println!("Enter the correct answer! (press Enter after typing)");
+    let answer = read_buffer();
+    let post_correct_url = create_url("/corrects".to_string()).unwrap();
+    let request_builder: RequestBuilder = client.post(post_correct_url).body(answer);
+    let body = request_builder.send().await?.text().await?;
+    println!("{}", body); //TODO: Unnecessary code to be deleted later
+
+    //TODO: Implement a hint posting function.
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,26 +1,44 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+struct GameMaster {
+    question: Mutex<String>,
+    answer: Mutex<String>,
+    // hint: Mutex<String>, //TODO: Implement a hint.
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
+#[post("/questions")]
+async fn create_question(req_body: String, game_master: web::Data<GameMaster>) -> impl Responder {
+    let mut question = game_master.question.lock().unwrap();
+    *question = req_body.clone();
+    HttpResponse::Ok().body(format!("Question set: {:?}", *question))
+}
+
+#[post("/corrects")]
+async fn create_correct(req_body: String, game_master: web::Data<GameMaster>) -> impl Responder {
+    let mut answer = game_master.answer.lock().unwrap();
+    *answer = req_body.clone();
+    HttpResponse::Ok().body(format!("Answer set: {:?}", *answer))
+}
+
+#[post("/answers")]
+async fn send_answer(req_body: String) -> impl Responder {
+    //TODO: 回答送信の実装
     HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let game_master = web::Data::new(GameMaster {
+        question: Mutex::new("".to_string()),
+        answer: Mutex::new("".to_string()),
+    });
+    HttpServer::new(move || {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .app_data(game_master.clone())
+            .service(create_question)
+            .service(create_correct)
+            .service(send_answer)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
